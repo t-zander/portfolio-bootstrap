27bc78110945cc27f0747dcd322a0b555c3c5ed8
t-zander <alex.sergeevich.tarasenko@gmail.com>
2019-05-12 18:09:23 +0300

    fixed typos

--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,7 +1,7 @@
 /*
   Rename this file to environment.ts
   and use it to store your global settings like base urls
-  this is using for local development
+  this is used for local development
 */
 export const environment = {
   production: false
