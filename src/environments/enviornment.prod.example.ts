edcfe45184fa0bec179ca495d1b396017236398a
t-zander <alex.sergeevich.tarasenko@gmail.com>
2019-05-12 18:07:56 +0300

    added blueprints for environment files

---
+++ src/environments/enviornment.prod.example.ts
@@ -0,0 +1,8 @@
+/*
+  Rename this file to environment.prod.ts
+  and use it to store your global settings like base urls
+  This is used during ng build --prod
+*/
+export const environment = {
+  production: true
+};
--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,3 +1,8 @@
+/*
+  Rename this file to environment.ts
+  and use it to store your global settings like base urls
+  this is using for local development
+*/
 export const environment = {
   production: false
 };
